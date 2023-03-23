fd272d94897e2e23f8da4a29bfeb2053e4da261b
Kat Seitz <katharina.seitz@northwestern.edu>
2023-03-23 09:54:00 -0500

    moving dwi

--- scripts/1_nifti_conversion/nifit_to_folder.py
+++ scripts/1_nifti_conversion/nifit_to_folder.py
@@ -25,7 +25,7 @@ def makedir(participant, directory):
     if(not os.path.exists(directory + "/fmap/")):
         os.mkdir(directory + "/fmap/")
     if(not os.path.exists(directory + "/dti/")):
-        os.mkdir(directory + "/dti/")
+        os.mkdir(directory + "/dwi/")
 
 '''
     Renames files all per the file renaming document from Nina
@@ -58,11 +58,12 @@ def rename_partic(participant, directory):
         counter = counter+1
 
     files = glob.glob(directory + "/" + participant + "--SpinEchoFieldMap_PA*")
-    pa_dict = []
+    pa_dict = {}
     for file in files:
         index1 = file.index("PA") + 5
         index2 = file.index("--e1")
         scan_number = int(file[index1:index2])
+
         pa_dict[scan_number] = file
     counter = 1
     for scan, file in sorted(pa_dict.items()):
@@ -73,14 +74,14 @@ def rename_partic(participant, directory):
         os.rename(file, directory + "/" + new_name) 
         counter = counter + 1
     #MID
-    files = glob.glob(directory + "/" + participant + "--MID1--*") + glob.glob(directory + "/" + participant + "--MID1*")
+    files = glob.glob(directory + "/" + participant + "--MID1--*") 
     for file in files:
         print(file)
         parts = file.split(".", 1)
         new_name = "sub-" + participant + "_ses-1_task-mid_run-01_bold." + parts[1]
         print(directory + "/" + new_name)
         os.rename(file, directory + "/" + new_name) 
-    files = glob.glob(directory + "/" + participant + "--MID2--*") + glob.glob(directory + "/" + participant + "--MID2*")
+    files = glob.glob(directory + "/" + participant + "--MID2--*") 
     for file in files:
         print(file)
         parts = file.split(".", 1)
@@ -153,6 +154,13 @@ def move_to_folders(participant, directory):
         print(file)
         parts = file.split("/")
         os.rename(file, directory + "/anat/" + parts[-1])
+    #move dwi
+    dwi_pattern = "sub-"+ participant + "_ses-1_dwi"
+    dwi_files = glob.glob(directory + "/" + dwi_pattern)
+    for file in dwi_files:
+        print(file)
+        parts = file.split("/")
+        os.rename(file, directory + "/dwi/" + parts[-1])
     #move fmap files
     fmap_files = glob.glob(directory + "/sub*")
     for file in fmap_files:
